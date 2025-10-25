8b44a0bc519ba542f1be4c43246b4940f9ee00ad
Nimira43 <n.rabone72@aol.com>
2025-10-25 10:43:37 +0100

    Tailwind 3 set up.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#ff4500',
+  			'support': '#008080',
+  			'dark': '#111',
+  			'light': '#fffdfa',
+  			'grey-1': '#333',
+  			'grey-2': '#999',
+  			'grey-3': '#ccc',
+  			'grey-4': '#eee',
+        'gold': '#ffd700'
+      },
+    },
+  },
+  plugins: [],
+}
+
