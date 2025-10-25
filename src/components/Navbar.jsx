dec499e1cdb3f52125bdb3c81c373ab3b5941444
Nimira43 <n.rabone72@aol.com>
2025-10-25 18:38:41 +0100

    searchModal

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { RxHamburgerMenu } from "react-icons/rx"
+import { Button } from './ui/button'
 
 const Navbar = () => {
   const [searchModal, setSearchModal] = useState(false)
@@ -8,9 +9,17 @@ const Navbar = () => {
     <div className='sticky top-0 z-60'>
       <nav className={`navbar-wrapper`}>
         <div className='flex items-center gap-3 md:gap-5'>
-          <button className='group main-btn transition-btn'>
-            <RxHamburgerMenu className='text-light' />
+          <button className='main-btn transition-btn'>
+            <RxHamburgerMenu 
+              className='text-light' 
+              size={24}
+              strokeWidth={1}  
+            />
           </button>
+
+          {searchModal && (
+            <div className='flex items-start justify-start fixed md:static top-0 z-100 left-0 bg-main md:bg-main-light md:dark:bg-main-dark w-full h-[100vh] md:h-fit p-5 md:p-0'></div>
+          )}
         </div>
       </nav>
     </div>
