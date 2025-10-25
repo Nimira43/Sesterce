6abf989d313a802b787d9dcaeb33f73531b74861
Nimira43 <n.rabone72@aol.com>
2025-10-25 11:00:23 +0100

    Tailwind up and running. Created logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { GiAbstract056 } from 'react-icons/gi'
+
 function App() {
   return (
-    <div>
-      <h1>Sesterce</h1>      
+    <div className='bg-dark p-6 flex gap-2 items-center'>
+      <GiAbstract056 className='text-gold text-4xl' />
+      <h1 className='text-gold text-4xl logo-font'>Sesterce</h1>      
     </div>
   )
 }
