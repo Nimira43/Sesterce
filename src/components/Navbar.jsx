54992ad6566bf235560219431102b45c4c87947e
Nimira43 <n.rabone72@aol.com>
2025-10-25 14:57:55 +0100

    Navbar - 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ const Navbar = () => {
   const [searchModal, setSearchModal] = useState(false)
 
   return (
-    <div>
-      
+    <div className='sticky top-0 z-60'>
+      <nav className={`navbar-wrapper`}>
+        <div className='flex items-center gap-3 md:gap-5'></div>
+      </nav>
     </div>
   )
 }
