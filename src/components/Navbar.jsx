266e03dc3d04c7f0b81b9f92613797793c1ed9c0
Nimira43 <n.rabone72@aol.com>
2025-11-12 22:05:22 +0000

    Navbar work  - 3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { useState, useEffect, useRef } from 'react'
 import { IoSearchOutline } from 'react-icons/io5'
-import { RxHamburgerMenu } from "react-icons/rx"
+import { RxHamburgerMenu, RxWidth } from "react-icons/rx"
 
 const Navbar = () => {
   const [searchModal, setSearchModal] = useState(false) 
   return (
-    <div className='stickey top-0 z-60'>
+    <div className='sticky top-0 z-60'>
       <nav className={`flex items-center justify-between border-b border-gray-100 dark:border-gray-800 dark:shadow-lg mb-4 bg-white dark:bg-gray-900 p-3 md:py-2 w-full`}>
         <div class='flex items-center gap-3 md:gap-5'>
-          <button className='group h-fit cursor-pointer rounded-full p-2 bg-none hover:bg-orange-100 dark:hover:bg-greay-800 opacoty-100'>
-            
+          <button className='group h-fit cursor-pointer rounded-full p-2 bg-none hover:bg-orange-100 dark:hover:bg-gray-800 opacity-100'>
+            <RxHamburgerMenu
+              size={24}
+              strokeWidth={1}
+              className='group-hover:text-orange-500 dark:text-gray-100 dark:group:hover:text-orange-500 text-black'
+            />
           </button>
         </div>
       </nav>      
