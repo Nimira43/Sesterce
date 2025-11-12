b85c760e15ab57ab926b47742f06eacd1255555f
Nimira43 <n.rabone72@aol.com>
2025-11-12 21:44:52 +0000

    searchModal state variable.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { IoSearchOutline } from 'react-icons/io5'
 import { RxHamburgerMenu } from "react-icons/rx"
 
 const Navbar = () => {
-  
+  const [searchModal, setSearchModal] = useState(false) 
   return (
     <div>
       Navbar      
