98a28cba4aaf426ecc315aa1db7a1f50b8c6983f
Nimira43 <n.rabone72@aol.com>
2025-11-12 21:50:13 +0000

    Nav element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,10 @@ import { RxHamburgerMenu } from "react-icons/rx"
 const Navbar = () => {
   const [searchModal, setSearchModal] = useState(false) 
   return (
-    <div>
-      Navbar      
+    <div className='stickey top-0 z-60'>
+      <nav className={`flex items-center justify-between border-b border-gray-100 dark:border-gray-800 dark:shadow-lg mb-4 bg-white dark:bg-gray-900 p-3 md:py-2 w-full`}>
+        
+      </nav>      
     </div>
   )
 }
