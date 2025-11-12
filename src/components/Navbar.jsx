089658f3bf3a6ed1834b693c9a8a0ebbcef35a76
Nimira43 <n.rabone72@aol.com>
2025-11-12 21:54:57 +0000

    Navbar work  - 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,11 @@ const Navbar = () => {
   return (
     <div className='stickey top-0 z-60'>
       <nav className={`flex items-center justify-between border-b border-gray-100 dark:border-gray-800 dark:shadow-lg mb-4 bg-white dark:bg-gray-900 p-3 md:py-2 w-full`}>
-        
+        <div class='flex items-center gap-3 md:gap-5'>
+          <button className='group h-fit cursor-pointer rounded-full p-2 bg-none hover:bg-orange-100 dark:hover:bg-greay-800 opacoty-100'>
+            
+          </button>
+        </div>
       </nav>      
     </div>
   )
