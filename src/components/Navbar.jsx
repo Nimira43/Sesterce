65a9bd00d541a545cbdbbebfb1392f3119f6a76f
Nimira43 <n.rabone72@aol.com>
2025-11-12 21:43:48 +0000

    Imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,9 @@
+import { useState, useEffect, useRef } from 'react'
+import { IoSearchOutline } from 'react-icons/io5'
+import { RxHamburgerMenu } from "react-icons/rx"
+
 const Navbar = () => {
+  
   return (
     <div>
       Navbar      
