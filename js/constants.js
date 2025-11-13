dbb77bfc4b9aa6d7f0bcfecc3d60c0c7a75e10ff
Nimira43 <n.rabone72@aol.com>
2025-11-13 00:09:42 +0000

    Constants - 2

--- js/constants.js
+++ js/constants.js
@@ -6,3 +6,8 @@ const MARGIN = 0.02
 const COLOUR_BG = '#111'
 const COLOUR_FRAME = '#000'
 const COLOUR_FRAME_BOTTOM = '#222'
+const COLOUR_RI = '#ff8c00'
+const COLOUR_RI_DARK = '#ff4500'
+const COLOUR_AI = '#ff0000'
+const COLOUR_AI_DARK = '#8b0000'
+
